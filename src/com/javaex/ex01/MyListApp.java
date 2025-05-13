a283ed5080d34348c968249282778acf163fe5a5
kangsb-010313 <kangsb668@naver.com>
2025-05-13 09:46:28 +0900

    chap04 250513 09:46

---
+++ src/com/javaex/ex01/MyListApp.java
@@ -0,0 +1,33 @@
+package com.javaex.ex01;
+
+public class MyListApp {
+
+	public static void main(String[] args) {
+		
+		//배열
+		Point[] pArray = new Point[3];
+		
+		//포인트
+		Point p01 = new Point(2,2);
+		Point p02 = new Point(3,3);
+		Point p03 = new Point(4,4);
+		
+		//배열에 포인트 주소 담기
+		pArray[0] = p01;
+		pArray[1] = p02;
+		pArray[2] = p03;
+		
+		System.out.println(pArray[0]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
